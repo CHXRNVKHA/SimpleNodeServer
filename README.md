33c30da7e2499667b8ada435553d3b6fb3a32fcd
WonderXuy <veko.dmitriy0@gmail.com>
2020-03-19 15:11:59 +0300

    SimpleNodeServer/feat: create main app and functionality

---
+++ README.md
@@ -0,0 +1,10 @@
+# Simple Node Server
+## Tutorial
+1. Intall [Node.js]:https://nodejs.org/en/, first of all
+1. Clone this repository by using command **git clone**
+1. Start this app by using command **node app.js** in cosole
+1. Then use those links:
+  1. http://127.0.0.1:3020/ or http://127.0.0.1:3020/index.html - you will see main page
+  1. http://127.0.0.1:3020/about.html - you will see about page
+  1. http://127.0.0.1:3020/contact.html - you will see contact page
+**Enjoy :)**
---
+++ app.js
@@ -0,0 +1,52 @@
+const http = require('http');
+const fs = require('fs');
+const hostname = '127.0.0.1';
+const port = 3020;
+
+const notFoundRes = function (res) {
+   res.statusCode = 404;
+   res.end('<h1>Resourse not found!</h1>');
+}
+const showFile = function (filePath, res) {
+   if (filePath === '/') {
+      filePath = 'index.html';
+   }
+   else {
+      filePath = filePath.substr(1);
+   }
+   fs.readFile(filePath, (err, data) => {
+      if (err) {
+         notFoundRes(res);
+      }
+      else {
+         res.end(data);
+      }
+   })
+}
+
+const server = http.createServer((req, res) => {
+   res.setHeader('Content-Type', 'text/html; charset=utf-8;');
+   const filePath = req.url;
+   switch (filePath) {
+      case '/':
+      case '/index.html': {
+         showFile(filePath, res);
+         break;
+      }
+      case '/about.html': {
+         showFile(filePath, res);
+         break;
+      }
+      case '/contact.html': {
+         showFile(filePath, res);
+         break;
+      }
+      default: {
+         notFoundRes(res);
+         break;
+      }
+   }
+});
+server.listen(port, hostname, () => {
+   console.log(`Server running at http://${hostname}:${port}/`);
+});
